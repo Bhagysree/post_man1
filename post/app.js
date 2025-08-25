6665a94527f899af7e4ad3a6fbb4b0e4d5ace11d
Bhagyasree <tellibhagyasree17@gmail.com>
2025-08-25 09:14:35 +0000

    its postman project

---
+++ post/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const app = express()
+const PORT = 5000
+
+app.get("/", (req, res) => {
+    res.send('Hello, server!')
+ });
+
+ app.get("/global", (req, res) => {
+    res.send('hello global server!')
+ });
+
+app.post("/register", (req, res) => {
+    let user = req.body;
+    console.log(user);
+    res.send('User registered successfully!');
+});
+
+app.post("/student/:rollno", (req, res) => {
+    let id = req.params.rollno;
+    console.log(`Student ID: ${id}`);
+    res.json(id)
+});
+app.put("/update", (req, res) => {
+   let updateData = req.body;
+    console.log(updateData);
+});
+
+app.delete("/delete", (req, res) => {
+    let deleteData = req.body;
+    console.log(deleteData);
+    res.send('Data deleted successfully!');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
